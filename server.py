a96f23aeabd319b69003c800a88d6246f2c0ed10
Osminalx <verdestro64@gmail.com>
2024-02-17 14:55:52 -0800

    Created chatManager class

--- server.py
+++ server.py
@@ -1,25 +1,30 @@
 import socket
 import threading
 
-def handle_client(client_socket):
-    while True:
-        try:
-            message = client_socket.recv(1024)
-            if not message:
-                break
-            print("Mensaje recibido:", message.decode())
-            broadcast(message, client_socket)
-        except Exception as e:
-            print("Error:", e)
-            break
+class chatManager:
+    def __init__(self) -> None:
+        pass
 
-def broadcast(message, sender_socket):
-    for client in clients:
-        if client != sender_socket:
+    def _broadcast(self, message, sender_socket):
+        for client in self.clients:
+            if client != sender_socket:
+                try:
+                    client.send(message)
+                except:
+                    self.clients.remove(client)
+
+    def handle_client(self, client_socket):
+        while True:
             try:
-                client.send(message)
-            except:
-                clients.remove(client)
+                message = client_socket.recv(1024)
+                if not message:
+                    break
+                print("Mensaje recibido:", message.decode())
+                self._broadcast(message, client_socket)
+            except Exception as e:
+                print("Error:", e)
+                break
+
 
 my_socket = socket.socket()
 my_socket.bind(('localhost', 8000))
@@ -28,12 +33,13 @@ my_socket.listen(5)
 
 print("Esperando conexiones...")
 
-clients = []
+chat_manager = chatManager()
+chat_manager.clients = []
 
 while True:
     client_socket, addr = my_socket.accept()
     print("Cliente conectado desde:", addr)
-    clients.append(client_socket)
+    chat_manager.clients.append(client_socket)
 
-    client_handler = threading.Thread(target=handle_client, args=(client_socket,))
+    client_handler = threading.Thread(target=chat_manager.handle_client, args=(client_socket,))
     client_handler.start()
